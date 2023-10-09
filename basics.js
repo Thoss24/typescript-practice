f7083db6ee5abbe2ce642b215ad362bbdd784019
Thoss24 <t.fogarty24@outlook.com>
2023-10-09 20:48:04 +0100

    union types/ generics / inference

--- basics.js
+++ basics.js
@@ -1,6 +1,37 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var age;
 age = 22;
 var userName;
 userName = "Thomas";
 var isStudent;
 isStudent = true;
+var obj;
+obj = {
+    name: "hello",
+    age: 12
+};
+// obj = {
+//     name: "hello",
+//     age: "12"
+// }
+var arr = [1, "test"]; // unison type
+var str = "test"; // type inference - whatever value a variable is declared as typescript will automatically declare it as that value type
+//str = 1
+var obj2;
+var people = {
+    name: "test1",
+    age: 28
+};
+function insertAtBeginning(array, value) {
+    var newArr = __spreadArray([value], array, true);
+    return newArr;
+}
+console.log(insertAtBeginning([2, 3, 4], 1));
--- basics.ts
+++ basics.ts
@@ -9,3 +9,48 @@ userName = "Thomas";
 let isStudent: boolean;
 
 isStudent = true
+
+let obj : {
+    name: string,
+    age: number
+};
+
+obj = {
+    name: "hello",
+    age: 12
+}
+
+// obj = {
+//     name: "hello",
+//     age: "12"
+// }
+
+let arr: (string|number)[] = [1, "test"] // unison type
+
+let str = "test" // type inference - whatever value a variable is declared as typescript will automatically declare it as that value type
+
+//str = 1
+
+let obj2: {
+    name: string;
+    age: number;
+}[];
+
+type Person = {
+    name: string;
+    age: number
+}
+
+let people: Person = {
+    name: "test1",
+    age: 28
+}
+
+function insertAtBeginning(array: any[], value: any) {
+    const newArr = [value, ...array]
+    return newArr
+}
+
+console.log(insertAtBeginning([2, 3, 4], 1));
+
+let numbers: number[] = [1, 2, 3]
